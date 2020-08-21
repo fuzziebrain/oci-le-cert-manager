ec73a99af8037a0ca860b4d45ed8bdaa2108dcff
Adrian Png <adrian.png@fuzziebrain.com>
2020-08-21 21:39:49 +0000

    Update main script

--- cert-manager.sh
+++ cert-manager.sh
@@ -2,11 +2,11 @@
 
 DIRNAME=$(dirname ${0})
 COMMAND=${1:-generate}
-ENV_FILE=${2:-}
+ENV_FILENAME=${2:-}
 
 docker run --rm \
   -p 8000:80 \
-  --env-file ${ENV_FIEL} \
+  --env-file ${DIRNAME}/${ENV_FILENAME} \
   -v $DIRNAME/letsencrypt:/etc/letsencrypt:z \
   -v $DIRNAME/.oci:/root/.oci \
   oci-le-cert-manager \
