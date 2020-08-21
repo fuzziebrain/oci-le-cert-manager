299e2efb0926a442d26ee02bbb85759bc6666cfc
Adrian Png <adrian.png@fuzziebrain.com>
2020-08-21 14:24:19 -0700

    Update path

--- cert-manager.sh
+++ cert-manager.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+DIRNAME=$(dirname ${0})
+COMMAND=${1:-generate}
+ENV_FILE=${2:-}
+
 docker run --rm \
   -p 8000:80 \
-  --env-file $PWD/domain.env \
-  -v $PWD/letsencrypt:/etc/letsencrypt:z \
-  -v $PWD/.oci:/root/.oci \
+  --env-file ${ENV_FIEL} \
+  -v $DIRNAME/letsencrypt:/etc/letsencrypt:z \
+  -v $DIRNAME/.oci:/root/.oci \
   oci-le-cert-manager \
-  cert-manager ${1:-generate}
+  cert-manager ${COMMAND}
